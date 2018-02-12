84b986eff36052ed5e1eae2b1941038d57291f58
vyastrub <yastrub27@gmail.com>
2018-02-13 00:55:50 +0200

    Add files via upload

---
+++ application(Windows_OS)/moc_form.cpp
@@ -0,0 +1,126 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'form.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../whiteboard/form.hpp"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'form.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.6.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Form_t {
+    QByteArrayData data[9];
+    char stringdata0[68];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Form_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Form_t qt_meta_stringdata_Form = {
+    {
+QT_MOC_LITERAL(0, 0, 4), // "Form"
+QT_MOC_LITERAL(1, 5, 12), // "change_color"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 1), // "i"
+QT_MOC_LITERAL(4, 21, 11), // "change_size"
+QT_MOC_LITERAL(5, 33, 1), // "s"
+QT_MOC_LITERAL(6, 35, 12), // "change_shape"
+QT_MOC_LITERAL(7, 48, 4), // "flag"
+QT_MOC_LITERAL(8, 53, 14) // "enable_manager"
+
+    },
+    "Form\0change_color\0\0i\0change_size\0s\0"
+    "change_shape\0flag\0enable_manager"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Form[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   34,    2, 0x08 /* Private */,
+       4,    1,   37,    2, 0x08 /* Private */,
+       6,    1,   40,    2, 0x08 /* Private */,
+       8,    0,   43,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Form::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Form *_t = static_cast<Form *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->change_color((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->change_size((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->change_shape((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->enable_manager(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject Form::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Form.data,
+      qt_meta_data_Form,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *Form::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Form::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_Form.stringdata0))
+        return static_cast<void*>(const_cast< Form*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int Form::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
